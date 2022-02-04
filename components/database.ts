b89c1a2e1a6b3d4c8011b66e150deda7d5d82c26
Apple <apple@Apples-MacBook-Pro.local>
2022-02-04 18:43:40 +0530

    database.ts refactored

--- components/database.ts
+++ components/database.ts
@@ -5,22 +5,14 @@ async function dbConnect() {
   // check if we have a connection to the database or if it's currently
   // connecting or disconnecting (readyState 1, 2 and 3)
   if (mongoose.connection.readyState >= 1) return
-  console.log("Process env",process.env);
   console.log(`Connecting to ${process.env.NODE_ENV} database...`)
-  return mongoose
-    .connect(process.env.MONGO_URL, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useFindAndModify: false,
-      // useCreateIndex: true,
-    })
-    .then(() => {
-      console.log('Connected to database!')
-    })
-    .catch(err => {
-      console.error(`Couldn't connect to database`, err)
-      return undefined
-    })
+  try{
+    await  mongoose.connect(process.env.MONGO_URL)
+    console.log('Connected to database!');
+  }catch(err){
+    console.log("Error connecting to database",err);
+    process.exit(1);
+  }
 }
 
 let connectionPromise = dbConnect;
