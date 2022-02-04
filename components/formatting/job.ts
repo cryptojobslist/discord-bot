d78923b6c18d04368660578f7eff724dab57ea29
Raman Shalupau <raman.shalupau@gmail.com>
2022-02-05 00:59:49 +0800

    improving job ad formatting

--- components/formatting/job.ts
+++ components/formatting/job.ts
@@ -33,6 +33,7 @@ export default function (job: Job) {
     ${jobLocation} ${salaryRange(job)}
 
     👇 Apply now or share with a friend:
-    [${job.canonicalURL}](${job.canonicalURL}?utm_source=DiscordBot&utm_medium=social)
+    ${job.canonicalURL}?utm_source=DiscordBot&utm_medium=social
   `.replace(/^ +/gm, '')
+  // [${job.canonicalURL}](${job.canonicalURL}?utm_source=DiscordBot&utm_medium=social)
 }
