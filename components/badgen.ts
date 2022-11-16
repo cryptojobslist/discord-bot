4504e4a2946aad75ff754f84f1164fc4d944cd5b
Raman Shalupau <raman.shalupau@gmail.com>
2022-11-16 13:39:55 +0800

    fetching discord bot stats with cache

--- components/badgen.ts
+++ components/badgen.ts
@@ -1,16 +1,34 @@
 import { Request, Response } from 'express'
 import { Client } from 'discord.js'
 
+type Stats = {
+  memberCount: number
+  serverCount: number
+}
+
+let cache: { data: Stats | any; createdAt: number } = { data: undefined, createdAt: 0 }
+async function getStatsWithCache(client: Client): Promise<Stats> {
+  return new Promise(async (resolve) => {
+    if (cache.data) resolve(cache.data)
+    if (!cache.data || Date.now() > cache.createdAt + 10_000) {
+      await client.guilds.fetch()
+      const stats = client.guilds.cache.reduce(
+        (sum, guild) => {
+          sum.memberCount += guild.memberCount
+          sum.serverCount += 1
+          return sum
+        },
+        { memberCount: 0, serverCount: 0 }
+      )
+      cache.data = stats
+      cache.createdAt = Date.now()
+    }
+    resolve(cache.data)
+  })
+}
+
 export default async function badgeN(req: Request, res: Response, client: Client) {
-  await client.guilds.fetch()
-  const stats = client.guilds.cache.reduce(
-    (sum, guild) => {
-      sum.memberCount += guild.memberCount
-      sum.serverCount += 1
-      return sum
-    },
-    { memberCount: 0, serverCount: 0 }
-  )
+  const stats: Stats = await getStatsWithCache(client)
 
   let result = {}
   if (req.params.type === 'servers') {
